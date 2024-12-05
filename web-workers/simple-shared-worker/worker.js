9d65eda7f590fe92e5046ca762504faa614fe873
Fighting <xing.suifeng@gmail.com>
2024-12-05 11:44:43 +0800

    Update worker.js

--- web-workers/simple-shared-worker/worker.js
+++ web-workers/simple-shared-worker/worker.js
@@ -1,8 +1,10 @@
+let lastData;
 onconnect = function (event) {
   const port = event.ports[0];
 
   port.onmessage = function (e) {
-    const workerResult = `Result: ${e.data[0] * e.data[1]}`;
+    const workerResult = `Result: ${e.data[0] * e.data[1]}, lastResult: ${lastData ? lastData[0] * lastData[1] : -1}`;
     port.postMessage(workerResult);
+    lastData = e.data;
   };
 };
